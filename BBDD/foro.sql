c1d88d86922f889e948a6cc6c9b382a76cc4396e
jorgemanuel-iesluisvives <jorgemanuel.cartategui@alumno.iesluisvives.org>
2025-12-22 12:42:17 +0100

    Añadido esqueleto del proyecto

---
+++ BBDD/foro.sql
@@ -0,0 +1,53 @@
+
+--Script de creacion de BBDD del proyecto
+
+-----------------------------------------------------------------
+/*
+Tabla 1
+    Columnas:
+        - Ejemplo : Tipo de dato
+        - Ejemplo : Tipo de dato
+        - Ejemplo : Tipo de dato
+        - Ejemplo : Tipo de dato
+*/
+
+
+
+
+
+
+-----------------------------------------------------------------
+
+-----------------------------------------------------------------
+/*
+Tabla 2
+    Columnas:
+        - Ejemplo : Tipo de dato
+        - Ejemplo : Tipo de dato
+        - Ejemplo : Tipo de dato
+        - Ejemplo : Tipo de dato
+*/
+
+
+
+
+
+
+-----------------------------------------------------------------
+
+-----------------------------------------------------------------
+/*
+Tabla 3
+    Columnas:
+        - Ejemplo : Tipo de dato
+        - Ejemplo : Tipo de dato
+        - Ejemplo : Tipo de dato
+        - Ejemplo : Tipo de dato
+*/
+
+
+
+
+
+
+-----------------------------------------------------------------
