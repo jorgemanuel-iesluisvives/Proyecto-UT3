1fef344aa7474ddeeef278b335196cf571f40aa7
jorgemanuel-iesluisvives <jorgemanuel.cartategui@alumno.iesluisvives.org>
2026-01-02 15:10:10 +0100

    Añadida la BBDD del proyecto

--- BBDD/foro.sql
+++ BBDD/foro.sql
@@ -1,53 +1,128 @@
 
 --Script de creacion de BBDD del proyecto
 
+-- Creacion de la base de datos
+------------------------------------------------------------------------
+DROP DATABASE IF EXISTS foro;
+CREATE DATABASE foro;
+USE foro;
+------------------------------------------------------------------------
+
+-- Creacion de tablas
+------------------------------------------------------------------------
+
 -----------------------------------------------------------------
+
 /*
-Tabla 1
+Tabla usuarios
     Columnas:
-        - Ejemplo : Tipo de dato
-        - Ejemplo : Tipo de dato
-        - Ejemplo : Tipo de dato
-        - Ejemplo : Tipo de dato
+        - id : INT
+        - password : VARCHAR
+        - nombre : VARCHAR
+        - foto : tinytext
+        - descripcion : text
+        - fecha_creacion : DATE
+        - rango : ENUM ("Administrador", "Moderador", "Usuario")
+        - estado : ENUM ("Activada", "Desactivada", "Baneada")
 */
 
-
-
-
-
+CREATE TABLE usuarios(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    password VARCHAR(255) NOT NULL,
+    nombre VARCHAR(100) NOT NULL UNIQUE,
+    foto tinytext,
+    descripcion text,
+    fecha_creacion DATE NOT NULL,
+    rango ENUM ("Administrador", "Moderador", "Usuario"),
+    estado ENUM ("Activada", "Desactivada", "Baneada")
+);
 
 -----------------------------------------------------------------
 
 -----------------------------------------------------------------
+
 /*
-Tabla 2
+Tabla posts
     Columnas:
-        - Ejemplo : Tipo de dato
-        - Ejemplo : Tipo de dato
-        - Ejemplo : Tipo de dato
-        - Ejemplo : Tipo de dato
+        - id : INT
+        - usuario : VARCHAR --Relacionado con usuarios/nombre
+        - titulo : VARCHAR
+        - fecha : DATE
+        - texto : text
+        - imagenes : TEXT
 */
 
+CREATE TABLE posts(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(100) NOT NULL, 
+    usuario VARCHAR(100) NOT NULL, 
+    fecha DATE NOT NULL,
+    texto text NOT NULL,
+    imagenes TEXT NOT NULL
 
-
-
-
+);
 
 -----------------------------------------------------------------
 
 -----------------------------------------------------------------
+
 /*
-Tabla 3
+Tabla threads
     Columnas:
-        - Ejemplo : Tipo de dato
-        - Ejemplo : Tipo de dato
-        - Ejemplo : Tipo de dato
-        - Ejemplo : Tipo de dato
+        - id : INT
+        - post : INT --Relacionado con posts/id
+        - usuario : VARCHAR --Relacionado con usuarios/nombre
+        - anterior : INT
+        - fecha : DATE
+        - texto : text
+
 */
 
-
-
-
-
+CREATE TABLE hilos(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    post INT NOT NULL, 
+    usuario VARCHAR(100) NOT NULL UNIQUE, 
+    anterior INT,
+    fecha DATE NOT NULL,
+    texto text NOT NULL
+);
 
 -----------------------------------------------------------------
+
+------------------------------------------------------------------------
+
+-- Creacion de relaciones de tablas
+------------------------------------------------------------------------
+
+-----------------------------------------------------------------
+
+/* 
+Relaciona la columna usuario de la tabla posts
+con la columna nombre de la tabla usuarios
+*/
+
+ALTER TABLE posts
+    ADD CONSTRAINT AUTOR_POST FOREIGN KEY (usuario) 
+    REFERENCES usuarios(nombre)
+    ON DELETE CASCADE ON UPDATE CASCADE;
+
+-----------------------------------------------------------------
+
+-----------------------------------------------------------------
+
+/* 
+Relaciona la columna usuario de la tabla posts
+con la columna nombre de la tabla usuarios
+*/
+
+ALTER TABLE hilos
+    ADD CONSTRAINT AUTOR_HILO FOREIGN KEY (usuario) 
+    REFERENCES usuarios(nombre)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+    ADD CONSTRAINT POST_HILO FOREIGN KEY (post) 
+    REFERENCES posts(id)
+    ON DELETE CASCADE ON UPDATE CASCADE;  
+
+-----------------------------------------------------------------
+
+------------------------------------------------------------------------
